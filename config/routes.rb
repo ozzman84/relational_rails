4cd84eb99b772c3b2e9cb5f1ae4b8ed2839fb675
Ozzie <mikeosmonson@gmail.com>
2021-08-24 20:06:20 -0600

    Add Show w/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  ##Beds
+  get '/beds', to: 'beds#index'
+  get '/beds/:id', to: 'beds#show'
+
+  ##Guests
+  get '/guests', to: 'guests#index'
+  get '/guests', to: 'guests#show'
 end
