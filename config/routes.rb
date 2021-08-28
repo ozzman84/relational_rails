4127c2d4cb7dab65f156c24d5fc03a67debbe65d
Ozzie <mikeosmonson@gmail.com>
2021-08-28 14:05:27 -0600

    Refator for tests.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  ##Welcome
+  get '/', to: 'welcome#index'
 
   ##Guests
   get '/guests', to: 'guests#index'
   get '/guests/:id', to: 'guests#show'
 
+  ##BedGuests
+  # get '/bedguests/:id/guests', to: 'bedguests#index'
+
   ##Beds
   get '/beds', to: 'beds#index'
   get '/beds/:id', to: 'beds#show'
+
+  ##BedGuests
   get '/beds/:id/guests', to: 'beds#guests_index'
 
-
-    #bakery
-
+  ##bakery
   get '/bakeries', to: 'bakeries#index'
 end
