1fed538a6081c57945c418d215c44779c863e15e
Ozzie <mikeosmonson@gmail.com>
2021-08-30 18:47:50 -0600

    Add boolean validation tests

--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -9,6 +9,7 @@ describe Guest, type: :model do
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
     it { should validate_presence_of(:rent) }
-    it { should validate_presence_of(:visiting) }
+    it { should allow_value(true).for (:visiting) }
+    it { should allow_value(false).for (:visiting) }
   end
 end
