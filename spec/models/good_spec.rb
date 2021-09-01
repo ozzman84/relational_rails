e073a79c630c7652c992d7d4c3437b6a6d1f045d
Erin Quinn <equinn125@gmail.com>
2021-08-31 22:44:36 -0600

    impl threshold activerecord method

--- spec/models/good_spec.rb
+++ spec/models/good_spec.rb
@@ -12,4 +12,26 @@ describe Good, type: :model do
     it { should validate_presence_of(:category)}
     it { should validate_presence_of(:days_old)}
   end
+
+  it 'returns pastries over a specified age' do
+    bakery_1 = Bakery.create!(name: "Taste of Denmark",
+      city: "Lakewood",
+      delivery: true,
+      review: 5
+    )
+    good_1 = bakery_1.goods.create!(name: "Cinnamon Roll",
+    category: "Pastry",
+    days_old: 0,
+    gluten_free: true,
+
+  )
+    good_2 = bakery_1.goods.create!(name: "Pumpkin Bread",
+    category: "Bread",
+    days_old: 1,
+    gluten_free: true,
+
+  )
+
+  expect(Good.all.over_day(0)).to eq([good_2])
+  end
 end
